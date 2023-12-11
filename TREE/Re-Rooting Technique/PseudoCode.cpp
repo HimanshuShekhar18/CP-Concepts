9e2749a7938d9e7d83182d0938bcf92aca4e8e26
Himanshu Shekhar <96346919+HimanshuShekhar18@users.noreply.github.com>
2023-12-11 08:52:34 +0530

    Update PseudoCode.cpp

--- TREE/Re-Rooting Technique/PseudoCode.cpp
+++ TREE/Re-Rooting Technique/PseudoCode.cpp
@@ -1,1 +1,18 @@
+void dfs2(int node, int par)
+{
+ int t1=dp[node];
+  for(auto it: adj[node])
+  {
+     if(it==par)
+        continue;
+    int t2=dp[it];
+   // transfer root from node to it
 
+    dfs(it,node);
+    // rollback root to node
+    // (restore old values)   
+   dp[node]=t1;
+   dp[it]=t2;
+ 
+  }
+}
